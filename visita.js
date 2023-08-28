b1b45c4d30dcc878646879f07bfb87994d25a568
ThaisCristinaNoemerg <141072308+ThaisCristinaNoemerg@users.noreply.github.com>
2023-08-28 14:45:56 -0400

    Create visita.js

---
+++ visita.js
@@ -0,0 +1,18 @@
+let currentImageIndex = 0;
+const images = document.querySelectorAll('.image');
+
+function changeImage(direction) {
+  currentImageIndex += direction;
+  if (currentImageIndex < 0) {
+    currentImageIndex = images.length - 1;
+  } else if (currentImageIndex >= images.length) {
+    currentImageIndex = 0;
+  }
+  showImage(currentImageIndex);
+}
+
+function showImage(index) {
+  images.forEach((image, i) => {
+    image.style.display = i === index ? 'block' : 'none';
+  });
+}
