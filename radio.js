cda66f08390f71ed0660dedc43571baef082490d
ThaisCristinaNoemerg <141072308+ThaisCristinaNoemerg@users.noreply.github.com>
2023-09-25 16:23:54 -0400

    Update radio.js

--- radio.js
+++ radio.js
@@ -1,9 +1,9 @@
 javascript
 // Obtenha o elemento de áudio pelo seu ID
-var audioPlayer = document.getElementById("audioPlayer");
+var playlist = document.getElementById("audioPlayer");
 
 // Adicione um evento "ended" ao elemento de áudio
-audioPlayer.addEventListener("ended", function() {
+playlist.addEventListener("ended", function() {
     // Quando a música atual terminar, carregue a próxima música
     loadNextSong();
 });
@@ -13,10 +13,18 @@ function loadNextSong() {
     // Aqui você pode adicionar a lógica para determinar qual é a próxima música a ser tocada
     // Por exemplo, você pode ter uma lista de músicas e simplesmente carregar a próxima música na lista
     var nextSong = "musica2.mp3";
+    var nextSong = "musica2.mp3";
+    var nextSong = "musica2.mp3";
+    var nextSong = "musica2.mp3";
+    var nextSong = "musica2.mp3";
+    var nextSong = "musica2.mp3";
+    var nextSong = "musica2.mp3";
+    var nextSong = "musica2.mp3";
+    var nextSong = "musica2.mp3";
 
     // Carregue a próxima música no elemento de áudio
-    audioPlayer.src = nextSong;
+    playlist.src = nextSong;
 
     // Inicie a reprodução da próxima música
-    audioPlayer.play();
+    playlist.play();
 }
