62580674268745b73afcd5314f99939edfe3fdd9
ThaisCristinaNoemerg <141072308+ThaisCristinaNoemerg@users.noreply.github.com>
2023-09-25 16:49:14 -0400

    Update radio.js

--- radio.js
+++ radio.js
@@ -1,3 +1,34 @@
+document.addEventListener("DOMContentLoaded
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Obtém todos os elementos de áudio da lista de reprodução
+    var audioElements = document.querySelectorAll("audio
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Obtém todos os elementos de áudio da lista de reprodução
+    var audioElements = document.querySelectorAll("audio");
+
+    // Inicializa a reprodução da primeira música
+    var currentIndex = 0;
+    audioElements[currentIndex].play();
+
+    // Função para tocar a próxima música
+    function playNext() {
+        // Pára o áudio atual
+        audioElements[currentIndex].pause();
+        // Move para a próxima música
+        currentIndex++;
+        // Verifica se ainda há músicas na lista
+        if (currentIndex < audioElements.length) {
+            // Toca a próxima música
+            audioElements[currentIndex].play();
+        }
+    }
+
+    // Adiciona um evento de término para cada elemento de áudio
+    audioElements.forEach(function (audio) {
+        audio.addEventListener("ended
+
 document.addEventListener("DOMContentLoaded", function () {
     // Obtém todos os elementos de áudio da lista de reprodução
     var audioElements = document.querySelectorAll("audio");
